edbdabd59513a32f0b450cdd88c16c0af9a37724
Mahesh-xr <mahesh.572xr@gmail.com>
2025-04-16 07:17:37 +0530

    complete sidebar component for both user logged in and not

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
 import { ThemeProvider } from "@/components/ThemeProvider";
 import Navbar from "@/components/Navbar";
+import Sidebar from "@/components/Sidebar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -37,7 +38,7 @@ export default function RootLayout({
               <main className="py-8">
                 <div className="max-w-7xl mx-auto px-4">
                   <div className="grid grid-cols-1 lg:grid-cols-12 gap-6">
-                    <div className="hidden lg:block lg: col-span-3 "> sidebar </div>
+                    <div className="hidden lg:block lg: col-span-3 "> <Sidebar/> </div>
                     <div className="lg:col-span-9">{children}</div>
                   </div>
                 </div>
--- components/action/user.action.ts
+++ components/action/user.action.ts
@@ -1,34 +1,49 @@
 "use server";
 import prisma from "@/lib/prisma";
 import { currentUser, auth } from "@clerk/nextjs/server";
-import { use } from "react";
 
 export async function syncUser() {
-
- try {
-    const {userId} = await auth()
-    const user = await currentUser()  
-    if(!user || !userId) return;
+  try {
+    const { userId } = await auth();
+    const user = await currentUser();
+    if (!user || !userId) return;
 
     const existingUser = await prisma.user.findUnique({
-        where:{
-            clerkId: userId
-        },
+      where: {
+        clerkId: userId,
+      },
     });
-    if(existingUser) return existingUser
+    if (existingUser) return existingUser;
     const dbUser = await prisma.user.create({
-        data:{
-            clerkId: userId,
-            name: `${user.firstName ||""} ${user.lastName || ""}`,
-            username: user.username?? user.emailAddresses[0].emailAddress.split("@")[0],
-            email: user.emailAddresses[0].emailAddress,
-            image: user.imageUrl
+      data: {
+        clerkId: userId,
+        name: `${user.firstName || ""} ${user.lastName || ""}`,
+        username:
+          user.username ?? user.emailAddresses[0].emailAddress.split("@")[0],
+        email: user.emailAddresses[0].emailAddress,
+        image: user.imageUrl,
+      },
+    });
+  } catch (error) {
+    console.log("Error in Sync User", error);
+  }
+}
+
+export async function getUserByClerkId(clerkId:string) {
+
+    return prisma.user.findUnique({
+        where:{
+            clerkId,
+        },
+        include:{
+            _count:{
+                select:{
+                    followers:true,
+                    following:true,
+                    posts:true
+                }
+            }
         }
     })
-    
- } catch (error) {
-    
- }
-
     
 }
