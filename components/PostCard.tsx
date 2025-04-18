a277a943a1b74bfccdc8c0140d053e26dab955c9
Mahesh-xr <mahesh.572xr@gmail.com>
2025-04-18 16:45:04 +0530

    add functionality for show post delete post

--- components/PostCard.tsx
+++ components/PostCard.tsx
@@ -1,6 +1,16 @@
-import { useUser } from '@clerk/nextjs';
+"use client";
+import { SignInButton, useUser } from '@clerk/nextjs';
 import React, { useState } from 'react'
-import { getPosts } from './action/post.action';
+import { createComment, deletePost, getPosts, toggleLike } from './action/post.action';
+import toast from 'react-hot-toast';
+import { Avatar, AvatarImage } from './ui/avatar';
+import { Textarea } from './ui/textarea';
+import { Button } from './ui/button';
+import {  HeartIcon, LogInIcon, MessageCircleIcon, SendIcon } from 'lucide-react';
+import { Card, CardContent } from './ui/card';
+import Link from 'next/link';
+import { DeleteAlertDialog } from './DeleteAlertDialog';
+import { formatDistanceToNow } from "date-fns";
 
 type Posts = Awaited<ReturnType<typeof getPosts>>
 type Post = Posts[number]
@@ -11,17 +21,100 @@ const PostCard =  ({post, dbUserId}:{post:Post, dbUserId:string | null}) => {
   const[isCommenting, setIsCommenting] = useState(false);
   const[isLiking, setIsLiking] = useState(false)
   const[isDeleting, setIsDeleting] = useState(false)
-  const[hasLiked, setHasLiked] = useState(false)
-  const[likes, setLikes] = useState(post._count.likes)
-  
-  
-  
-  return (
-    <div>
-
-      postcard
+  const[hasLiked, setHasLiked] = useState(post.likes.some(like => like.userId === dbUserId))
+  const[optLikes, setOptLikes] = useState(post._count.likes)
+  const handleLike = async () =>{
+    if(isLiking) return
+    try {
+      setIsLiking(true)
+      setHasLiked(prev => !prev)
+      setOptLikes(prev => prev + (hasLiked ? -1 : 1))
+      await toggleLike(post.id)
       
-    </div>
+    } catch (error) {
+      setOptLikes(post._count.likes)
+      setHasLiked(post.likes.some(like => like.userId === dbUserId))
+      
+    }
+    finally{
+      setIsLiking(false)
+    }
+  }
+
+  const handleAddComment = async () => {
+    if (!newComment.trim() || isCommenting) return;
+    try {
+      setIsCommenting(true);
+      const result = await createComment(post.id, newComment);
+      if (result?.success) {
+        toast.success("Comment posted successfully");
+        setNewComment("");
+      }
+    } catch (error) {
+      toast.error("Failed to add comment");
+    } finally {
+      setIsCommenting(false);
+    }
+  };
+
+  const handleDeletePost = async () => {
+    if (isDeleting) return;
+    try {
+      setIsDeleting(true);
+      const result = await deletePost(post.id);
+      if (result.success) toast.success("Post deleted successfully");
+      else throw new Error(result.error);
+    } catch (error) {
+      toast.error("Failed to delete post");
+    } finally {
+      setIsDeleting(false);
+    }
+  };
+  
+  
+ 
+
+  return (
+    <Card className="overflow-hidden">
+      <CardContent className="p-4 sm:p-6">
+        <div className="space-y-4">
+          <div className="flex space-x-3 sm:space-x-4">
+            <Link href={`/profile/${post.author.username}`}>
+              <Avatar className="size-8 sm:w-10 sm:h-10">
+                <AvatarImage src={post.author.image ?? "/avatar.png"} />
+              </Avatar>
+            </Link>
+
+            {/* POST HEADER & TEXT CONTENT */}
+            <div className="flex-1 min-w-0">
+              <div className="flex items-start justify-between">
+                <div className="flex flex-col sm:flex-row sm:items-center sm:space-x-2 truncate">
+                  <Link
+                    href={`/profile/${post.author.username}`}
+                    className="font-semibold truncate"
+                  >
+                    {post.author.name}
+                  </Link>
+                  <div className="flex items-center space-x-2 text-sm text-muted-foreground">
+                    <Link href={`/profile/${post.author.username}`}>@{post.author.username}</Link>
+                    <span>•</span>
+                    <span>{formatDistanceToNow(new Date(post.createdAt))} ago</span>
+                  </div>
+                </div>
+                {/* Check if current user is the post author */}
+                {dbUserId === post.author.id && (
+                  <DeleteAlertDialog isDeleting={isDeleting} onDelete={handleDeletePost} />
+                )}
+              </div>
+              <p className="mt-2 text-sm text-foreground break-words">{post.content}</p>
+            </div>
+          </div>
+
+       
+        </div> 
+      </CardContent>
+    </Card>
+
   )
 }
 
