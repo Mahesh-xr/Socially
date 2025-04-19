56b89ae60fdf5b36972adaf9db96080c856219d3
Mahesh-xr <mahesh.572xr@gmail.com>
2025-04-20 00:04:58 +0530

    add functionality for drag and drop

--- components/CreatePost.tsx
+++ components/CreatePost.tsx
@@ -8,6 +8,8 @@ import { ImageIcon, Loader2Icon, SendIcon } from "lucide-react";
 import { Button } from "./ui/button";
 import { createPost } from "./action/post.action";
 import toast from "react-hot-toast";
+import ImageUpload from "./ImageUpload";
+
 
 const CreatePost = () => {
   const { user } = useUser();
@@ -58,6 +60,22 @@ const CreatePost = () => {
               disabled={isPosting}
             />
           </div>
+
+       {/* Handle Image */}
+       {(showImageUpload || imageUrl)&& (
+        <div className="border rounded-lg p-4">
+          <ImageUpload
+          endpoint="postImage"
+          value={imageUrl}
+          onChange={url=>{
+            setImageUrl(url)
+            if(!url) setShowImageUpload(false)
+          }}
+          />
+        </div>
+       )}
+
+
           <div className="flex items-center justify-between border-t pt-4">
             <div className="flex space-x-2">
               <Button
